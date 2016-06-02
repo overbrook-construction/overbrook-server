ac7885373b0c087f8848e68a7411a05296148076
DAVID HEYDUCK <heyduckd@gmail.com>
2016-06-01 21:16:40 -0700

    allowing cors from *

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ require('./routes/addHomes-route')(apiRouter);
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, token, authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
+//app.use((req, res, next) => {
+//  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+//  res.header('Access-Control-Allow-Headers', 'Content-Type, token, authorization');
+//  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//  next();
+//});
 
 app.use(bodyParser.json());
 app.use('/', apiRouter);
