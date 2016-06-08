866935a5732bdf3ed419354842ac512fe55194fa
Sam Gruse <sgruse89@gmail.com>
2016-06-08 16:53:54 -0700

    Commit before adding the file uploader functionality

--- server.js
+++ server.js
@@ -24,14 +24,6 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-//app.use((req, res, next) => {
-//  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-//  res.header('Access-Control-Allow-Headers', 'Content-Type, token, authorization');
-//  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//  next();
-//});
-
 app.use(bodyParser.json());
 app.use('/', apiRouter);
 
